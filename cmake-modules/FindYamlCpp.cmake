f142a1aa3b6af506e9bcba72baae3bfba95b3c6d
fabian <rookfighter@develop>
2014-10-17 10:22:32 +0200

    finding yaml-cpp works

--- cmake-modules/FindYamlCpp.cmake
+++ cmake-modules/FindYamlCpp.cmake
@@ -3,3 +3,20 @@
 # YAMLCPP_INCLUDE_DIR
 # YAMLCPP_LIBRARY
 # YAMLCPP_FOUND
+
+find_path(YAMLCPP_INCLUDE_DIR yaml-cpp/yaml.h
+          HINTS "${YAMLCPP_ROOT}/include"
+)
+
+find_library(YAMLCPP_LIBRARY NAMES yaml-cpp
+             HINTS
+             "${YAMLCPP_ROOT}/bin"
+             "${YAMLCPP_ROOT}/lib"
+             "${YAMLCPP_ROOT}/build")
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set LIBXML2_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(YamlCpp  DEFAULT_MSG
+                                  YAMLCPP_LIBRARY
+                                  YAMLCPP_INCLUDE_DIR)

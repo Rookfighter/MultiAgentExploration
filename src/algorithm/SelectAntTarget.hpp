14359e4ff05e424a846410abd4f7d1fc4ad90b6e
fabian <rookfighter@develop>
2014-10-18 16:13:22 +0200

    implemented marker search

--- src/algorithm/SelectAntTarget.hpp
+++ src/algorithm/SelectAntTarget.hpp
@@ -6,25 +6,29 @@
 
 namespace mae
 {
-	
+
 	typedef struct {
 		Marker *marker;
 		double angleToTurn;
 	} AntTarget;
-	
+
 	class SelectAntTarget
 	{
 	private:
 		ExplorationBot &robot_;
-		bool directionMarked_[4];
 		std::vector<Marker*> markerInRange_;
+		AntTarget target_; 
 		
-		void getMarkerInRange(const std::vector<Marker*> &p_availableMarker);
+		void getMarkerInRange(Marker *p_currentMarker,
+		                      const std::vector<Marker*> &p_availableMarker);
+		bool checkBlankSpace();
+		void getMarkerTarget();
 	public:
 		SelectAntTarget(ExplorationBot &p_robot);
 		~SelectAntTarget();
-		
-		AntTarget execute(const std::vector<Marker*> &p_availableMarker);
+
+		AntTarget execute(Marker *p_currentMarker,
+		                  const std::vector<Marker*> &p_availableMarker);
 
 	};
 

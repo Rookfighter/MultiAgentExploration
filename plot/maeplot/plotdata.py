77fef555382e66ec7fdbcd569cf80f506a05321c
Rookfighter <rookfighter@develop>
2014-12-11 13:13:30 +0100

    done some refactoring of barchart plotting

--- plot/maeplot/plotdata.py
+++ plot/maeplot/plotdata.py
@@ -1,4 +1,17 @@
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
+def getBarChartColorCyle():
+    colorCycle = []
+    alphaVal = 0.6
+    colConv = colors.ColorConverter()
+    colorCycle.append(colConv.to_rgba('red', alphaVal))
+    colorCycle.append(colConv.to_rgba('blue', alphaVal))
+    colorCycle.append(colConv.to_rgba('orange', alphaVal))
+    colorCycle.append(colConv.to_rgba('olive', alphaVal))
+    colorCycle.append(colConv.to_rgba('cyan', alphaVal))
+    
+    return colorCycle
 
 def usecToMin(usec):
     return float(usec / 1000) / 60000
@@ -9,7 +22,6 @@ def plotCoverageEventsPerCount(data, outfile):
     plt.xlabel('minutes')
     plt.ylabel('coverage')
     plt.title('Coverage per amount of Robots')
-    plt.grid(True)
     
     for robotCount in sorted(data):
         meanData = data[robotCount].getMean()
@@ -24,15 +36,15 @@ def plotCoverageEventsPerCount(data, outfile):
     
     plt.legend(loc='lower right')
     plt.savefig(outfile)
-
-def plotFinalCoverageTimes(data, outfile):
+    
+def plotFinalCoverageBarchart(ylabel, title, data, dataIndex, outfile):
     plt.figure()
     plt.cla()
     
     fig, ax = plt.subplots()
     ax.set_ylabel('minutes')
     ax.set_title('Time to Coverage')
-    colorCycle = ['red', 'blue', 'orange', 'olive', 'cyan']; 
+    colorCycle = getBarChartColorCyle();
     
     availableWorldsTmp = set()
     for worldDict in data.values():
@@ -40,14 +52,18 @@ def plotFinalCoverageTimes(data, outfile):
             availableWorldsTmp.add(worldType)
     
     availableWorlds = sorted(availableWorldsTmp)
-    width = 0.1
+    width = 1.0 / (len(data) + 2)
     
     algorithmCount = 0
     for algorithm, worldDict in data.iteritems():
         toPlot = []
         for worldType in availableWorlds:
             if worldType in worldDict:
-                toPlot.append(usecToMin(worldDict[worldType].getMean()[1][0]))
+                dataVal = worldDict[worldType].getMean()[dataIndex][0]
+                if dataIndex == 1:
+                    toPlot.append(usecToMin(dataVal))
+                else:
+                    toPlot.append(dataVal)
             else:
                 toPlot.append(0.0)
         
@@ -61,40 +77,9 @@ def plotFinalCoverageTimes(data, outfile):
     
     fig.set_size_inches(18.5,10.5)
     plt.savefig(outfile, dpi=100)
+
+def plotFinalCoverageTimes(data, outfile):
+    plotFinalCoverageBarchart('minutes', 'Time to Coverage', data, 1, outfile)
     
 def plotFinalCoverage(data, outfile):
-    plt.figure()
-    plt.cla()
-    
-    fig, ax = plt.subplots()
-    ax.set_ylabel('coverage')
-    ax.set_title('Coverage in Time')
-    colorCycle = ['red', 'blue', 'orange', 'olive', 'cyan']; 
-    
-    availableWorldsTmp = set()
-    for worldDict in data.values():
-        for worldType in worldDict:
-            availableWorldsTmp.add(worldType)
-    
-    availableWorlds = sorted(availableWorldsTmp)
-    width = 0.1
-    
-    algorithmCount = 0
-    for algorithm, worldDict in data.iteritems():
-        toPlot = []
-        for worldType in availableWorlds:
-            if worldType in worldDict:
-                toPlot.append(worldDict[worldType].getMean()[0][0])
-            else:
-                toPlot.append(0.0)
-        
-        leftBorders = [i + (algorithmCount * width) for i in xrange(len(toPlot))]
-        ax.bar(leftBorders, toPlot, width=width, label=algorithm, color=colorCycle[algorithmCount % len(colorCycle)])
-        algorithmCount = algorithmCount + 1
-        
-    ax.set_xticks([(i + (len(availableWorlds) * width) / 2) for i in xrange(len(availableWorlds))])
-    ax.set_xticklabels(availableWorlds)
-    ax.legend(loc='upper right')
-    
-    fig.set_size_inches(18.5,10.5)
-    plt.savefig(outfile, dpi=100)
+    plotFinalCoverageBarchart('coverage', 'Coverage in Time', data, 0, outfile)

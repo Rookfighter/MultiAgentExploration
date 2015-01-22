ede158e7437cc3953e8b2df96f0dc5fd86416e60
fabian <rookfighter@develop>
2015-01-22 12:05:31 +0100

    removed unnecessary output

--- script/maeplot/experimentdir.py
+++ script/maeplot/experimentdir.py
@@ -52,9 +52,6 @@ class ExperimentDirectory:
         self.robotCount_ = int(dataFile.data_[0][0]);
         self.worldType_ = dataFile.data_[0][1];
         
-        if self.robotCount_ == 8:
-            print "ROBOTS"
-        
         # load final coverage
         filename = os.path.join(self.directory_, FINAL_COVERAGE_FILE)
         dataFile.load(filename)
